18e5d1fdc6022efce99e250f28715e0f0d9d257c
NoelGamaliel <techmbala@gmail.com>
2023-03-03 06:52:16 +0100

    Ajout the link for javaScript for the toggle button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,11 @@
 
+function myFunction() {
+    const btn_toggle = document.getElementById("mylinks");
+    if (btn_toggle.style.display === "block") {  
+        btn_toggle.style.display = "none";
+    } 
+    else {
+        btn_toggle.style.display = "block";
+    }
+  }
 
