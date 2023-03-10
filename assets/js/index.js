333b49970865402f9d281db3a2cf05266f90f818
NoelGamaliel <techmbala@gmail.com>
2023-03-10 09:39:28 +0100

    Modif of the script js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,13 @@
 
 function myFunction() {
     const btn_toggle = document.getElementById("mylinks");
-    if (btn_toggle.style.display === "block") {  
+
+    if (btn_toggle.style.display === "block") { 
+        
         btn_toggle.style.display = "none";
     } 
     else {
+        
         btn_toggle.style.display = "block";
     }
   }
